b6f0434278381845e350653f3feadc068b51b9bd
javiersolano010 <javisolanossr2@gmail.com>
2026-07-15 14:05:09 -0500

    Edit Test

---
+++ SistemaStreamFlow/src/test/java/com/streamflow/servicio/SuscripcionServicioTest.java
@@ -0,0 +1,58 @@
+package com.streamflow.servicio;
+
+import com.streamflow.dao.SuscripcionDAO;
+import com.streamflow.dao.SuscripcionDAOMemoria;
+import com.streamflow.modelo.CalidadStreaming;
+import com.streamflow.modelo.Suscripcion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class SuscripcionServicioTest {
+
+    private SuscripcionServicio suscripcionServicio;
+    private SuscripcionDAO suscripcionDAO;
+
+    @BeforeEach
+    void configurar() {
+        suscripcionDAO = new SuscripcionDAOMemoria();
+        suscripcionService = new SuscripcionService(suscripcionDAO);
+    }
+
+    @Test
+    void calcularCostoMensualSegunCalidad() {
+        assertEquals(4.99, suscripcionService.calcularCostoMensual(CalidadStreaming.SD));
+        assertEquals(8.99, suscripcionService.calcularCostoMensual(CalidadStreaming.HD));
+        assertEquals(13.99, suscripcionService.calcularCostoMensual(CalidadStreaming.UHD_4K));
+    }
+
+    @Test
+    void calcularCostoConDescuentoPorAntiguedad() {
+        double costoSinDescuento = suscripcionService.calcularCostoConDescuento(CalidadStreaming.HD, 2);
+        double costoDescuentoMedio = suscripcionService.calcularCostoConDescuento(CalidadStreaming.HD, 6);
+        double costoDescuentoAlto = suscripcionService.calcularCostoConDescuento(CalidadStreaming.HD, 12);
+
+        assertEquals(8.99, costoSinDescuento, 0.001);
+        assertEquals(8.09, costoDescuentoMedio, 0.01);
+        assertEquals(7.19, costoDescuentoAlto, 0.01);
+    }
+
+    @Test
+    void crearSuscripcionLaPersisteEnElDAO() {
+        Suscripcion suscripcion = suscripcionService.crearSuscripcion(1, 100, CalidadStreaming.UHD_4K);
+
+        assertEquals(1, suscripcionDAO.listarPorUsuario(100).size());
+        assertEquals(13.99, suscripcion.getCostoMensual());
+    }
+
+    @Test
+    void cancelarSuscripcionLaMarcaComoInactiva() {
+        Suscripcion suscripcion = suscripcionService.crearSuscripcion(1, 100, CalidadStreaming.SD);
+
+        suscripcionService.cancelarSuscripcion(suscripcion);
+
+        assertFalse(suscripcion.isActiva());
+    }
+}
